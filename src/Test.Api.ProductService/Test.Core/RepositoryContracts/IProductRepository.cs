824b5030682fb1bd71c17b6fb17e1fda612a58d2
arthur <arthur@flowfit.com>
2025-03-14 15:43:15 +0400

    Re-add Product Microservice project correctly

---
+++ src/Test.Api.ProductService/Test.Core/RepositoryContracts/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Test.Core.Entities;
+
+namespace Test.Core.RepositoryContracts;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<Product?> GetByIdAsync(Guid id);
+    Task<Product> AddAsync(Product product);
+    Task<Product> UpdateAsync(Product product);
+    Task<bool> DeleteAsync(Guid id);
+
+    Task UpdateProductStockAsync(Guid productId, int quantity);
+}
